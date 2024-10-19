491e71fb89298855fd825deae779d2f23f035d71
HK <179970062+HKM0@users.noreply.github.com>
2024-10-19 18:01:30 +0200

    Heki
    Bandi feladatát feldobtam progalapból

---
+++ 2024-1/prog_alap/beadando/1/jelenlevok/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+namespace Bandiszenved
+{
+    /*
+     * Egy rendezvényre N vendég érkezik, ismerjük mindegyikük érkezési és távozási idejét. 
+     * Számold meg, hogy a K. időpontban hány vendég van a rendezvényen!
+    */
+    internal class Program
+    {
+        const int MaxN = 100;
+
+        struct Vendégek
+        {
+            public int erkezes;
+            public int tavozas;
+        }
+
+        //beolvasás
+        static void beolvas(out int vendegszam, out int ido, ref Vendégek[] k)
+        {
+            Console.Error.Write("Kérem a [vendégek számát] [időt percben]: ");
+            string[] beállítások = Console.ReadLine().Split(' ');
+            vendegszam = Int32.Parse(beállítások[0]);
+            ido = Int32.Parse(beállítások[1]);
+
+            for (int i = 0; i < vendegszam; i++)
+            {
+                Console.Error.WriteLine($"Kérem a(z) {i + 1}. vendég: [érekzését] [távozását] ");
+                string[] tmp = Console.ReadLine().Split(' ');
+                k[i].erkezes = Int32.Parse(tmp[0]);
+                k[i].tavozas = Int32.Parse(tmp[1]);
+            }
+        }
+        //algoritmus
+        static int bentosszesen(ref Vendégek[] k, int vendegszam, int ido)
+        {
+            int bentivendegek = 0;
+
+            for (int i = 0; i < vendegszam; i++)
+            {
+                if (k[i].erkezes < ido && k[i].tavozas > ido)
+                {
+                    bentivendegek++;
+                }
+            }
+
+            return bentivendegek;
+        }
+
+        static void Main(string[] args)
+        {
+            // deklaracio
+            int vendegszam = 0;
+            int ido = 0;
+            Vendégek[] k = new Vendégek[MaxN];
+
+            beolvas(out vendegszam, out ido, ref k);
+
+            // kiiras
+            Console.WriteLine(bentosszesen(ref k, vendegszam, ido));
+
+        }
+    }
+}
