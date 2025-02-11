3b1a904657c191011397a6d6012f9ad68d7ebd98
HK <179970062+HKM0@users.noreply.github.com>
2025-02-11 12:41:47 +0100

    prognyelvek gyak01
    hiányos anyag

---
+++ 2024-2/prognyelvek/gyak/01/auto.java
@@ -0,0 +1,9 @@
+public class auto {
+    public int ulesDb=0;
+    public String rendszam="";
+
+    public void Auto (String rendszam, int ulesDb){
+        this.rendszam="rendszam";
+        this.ulesDb=0;
+    }
+}
---
+++ 2024-2/prognyelvek/gyak/01/main.java
@@ -0,0 +1,6 @@
+public class main{
+    int x=10;
+    public static void Main(String[] args){
+        System.out.println("Hello world");
+    }
+}
