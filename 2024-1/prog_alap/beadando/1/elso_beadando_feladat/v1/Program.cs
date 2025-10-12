c5eeacd2e1eb441d68d1c72b38ff9dd66eb17273
HK <179970062+HKM0@users.noreply.github.com>
2025-10-13 00:55:19 +0200

    progalap beadando 1 rendezve

---
+++ 2024-1/prog_alap/beadando/1/elso_beadando_feladat/v1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices;
+using System;
+namespace ElsoBeadandoFeladat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int.TryParse(Console.ReadLine(), out N);
+            int db=0;
+            for (int i = 0; i < N; i++)
+            {
+                 int kukutyin = int.Parse(kettoadat[0]);
+                int piripocs = int.Parse(kettoadat[1]);
+                if (kukutyin < piripocs)
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("{0}", db);
+        }
+    }
+}
---
+++ 2024-1/prog_alap/beadando/1/elso_beadando_feladat/v2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Elsobeadandomasodikverz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            int.TryParse(Console.ReadLine(), out N);
+
+            int[] Kukutyin = new int[N];
+            int[] Piripocs = new int[N];
+            int db = 0;
+            for (int i = 0; i < N; i++)
+            {
+                string[] kettoadat = Console.ReadLine().Split(' ');
+                Kukutyin[i] = int.Parse(kettoadat[0]);
+                Piripocs[i] = int.Parse(kettoadat[1]);
+                if (Kukutyin[i] < Piripocs[i])
+                {
+                    db++;
+                }
+            }
+            Console.WriteLine("{0}", db);
+        }
+    }
+}
